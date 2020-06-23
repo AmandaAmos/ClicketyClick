eabd72bb6160cc0682602a011f6f5a76117ca89d
Amanda Amos <59996872+AmandaAmos@users.noreply.github.com>
2020-06-22 18:40:17 -0700

    Updates

---
+++ precache-manifest.93202a40050e74ad57268ec14ebd7bb7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5df264cd612b7d292c41e0cc51989353",
+    "url": "/ClicketyClick/index.html"
+  },
+  {
+    "revision": "fdf5d9bac99f17b7e0ef",
+    "url": "/ClicketyClick/static/css/main.cc086bbd.chunk.css"
+  },
+  {
+    "revision": "1352616663ea628cc890",
+    "url": "/ClicketyClick/static/js/2.23f36dfe.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ClicketyClick/static/js/2.23f36dfe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fdf5d9bac99f17b7e0ef",
+    "url": "/ClicketyClick/static/js/main.2689a369.chunk.js"
+  },
+  {
+    "revision": "323cb824e823aa2875d0",
+    "url": "/ClicketyClick/static/js/runtime-main.fccdc2df.js"
+  }
+]);
